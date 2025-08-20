f447dbf1535b3b277b55860cca1f4ff674d8d854
suraj235 <er.surajsanwal@gmail.com>
2025-08-21 01:32:19 +0530

    exploring more about routed and middlewares

--- src/app.js
+++ src/app.js
@@ -3,14 +3,45 @@ const app = express();
 
 // replaced app.use() with app.get() for specific routes.
 // app.use() is for all routes.
-
-app.get('/suru', (req, res) => {
-  res.send('Hello from Suru!');
-});
+// + indicates the particular character or string can be repeated as many times as needed.
+// ? conditional, meaning the preceding character or group is optional.
+// * indicates the preceding character or group can be repeated as many times as needed.
+// handling routes with next().
+// Middleware functions can be used to handle requests before they reach the route handler.
 
 app.get('/', (req, res) => {
   res.send('Hello from DevTinder Homepage!');
 });
+
+app.get("/suru", (req, res, next) => {
+  next();
+  res.send('Hello from Suru!');
+});
+
+app.get("/suru", (req, res, next) => {
+  res.send('Hello from Suru2!');
+});
+
+// Auth  Middleware for All Requests
+app.use('/admin', (req, res, next) => {
+  console.log("Admin Middleware is running");
+  const token = 'suru'; // Simulated token for authentication
+  if (token !== 'suru') {
+    res.status(403).send('Forbidden: You do not have permission to access this resource.');
+  } else {
+    next();
+  }
+});
+
+app.get('/admin/getAllUsers', (req, res) => {
+  res.send('Hello from Admin Get All Users!');
+});
+
+app.get('/admin/getUser', (req, res) => {
+  // Logic to check authentication and authorization
+  res.send('Hello from Admin Get User of ID 1!');
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
